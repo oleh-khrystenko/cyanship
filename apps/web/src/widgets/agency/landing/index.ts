4073588935928a3ac4224017906e0ed05f310380
Oleh Khrystenko <olegkhristenko@gmail.com>
2026-03-30 12:16:16 +0300

    refactor(web): move demo video section into dedicated folder

--- apps/web/src/widgets/agency/landing/index.ts
+++ apps/web/src/widgets/agency/landing/index.ts
@@ -1,7 +1,7 @@
 export { HeroSection } from './HeroSection';
 export { ProblemSection } from './ProblemSection';
 export { DogfoodingSection } from './DogfoodingSection';
-export { DemoVideoSection } from './PortfolioSection';
+export { DemoVideoSection } from './DemoVideoSection';
 export { WorkflowSection } from './WorkflowSection';
 export { PricingSection } from './PricingSection';
 export { FooterCtaSection } from './FooterCtaSection';
