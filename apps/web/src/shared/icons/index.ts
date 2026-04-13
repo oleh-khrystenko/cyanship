450412214722bb46af8269a4f56bb1fe9c16c6ed
Oleh Khrystenko <olegkhristenko@gmail.com>
2026-04-13 17:11:02 +0300

    chore(branding): replace X links with Upwork

--- apps/web/src/shared/icons/index.ts
+++ apps/web/src/shared/icons/index.ts
@@ -1,6 +1,6 @@
 export { default as GitHubIcon } from './GitHubIcon';
 export { default as GoogleIcon } from './GoogleIcon';
 export { default as LinkedInIcon } from './LinkedInIcon';
-export { default as XIcon } from './XIcon';
+export { default as UpworkIcon } from './UpworkIcon';
 
 export type { IconProps } from './types';
